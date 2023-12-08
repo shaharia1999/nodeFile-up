63ed6ca125de23393d93b53fdde8990c0dc46d72
shaharia1999 <adeptshaharia@gmail.com>
2023-12-09 02:22:56 +0600

    unco

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ const uri = "mongodb+srv://shaharia:tM71pdsglZ08qlEh@cluster0.rgssr8j.mongodb.ne
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri
-//   , {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// }
+  , {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+}
 );
 
 async function run() {
